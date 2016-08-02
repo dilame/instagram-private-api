2cd342f032d54c4f36385a79435b2d25487c2690
Richard Hutta <huttarichard@gmail.com>
2016-08-02 19:52:59 +0200

    Helpers reference

--- index.js
+++ index.js
@@ -3,4 +3,5 @@ var InstagramPrivateAPI = {};
 
 InstagramPrivateAPI = {};
 InstagramPrivateAPI.V1 = require('./client/v1');
+InstagramPrivateAPI.Helpers = require('./helpers');
 module.exports = InstagramPrivateAPI;
