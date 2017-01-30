45630ad9c13b23b3929f6dbbc1ed93e324a8d051
Ivan <ivan_90.90@mail.ru>
2017-01-30 16:18:19 +0300

    Fix for 201 parseMiddleware statusCode

--- client/v1/request.js
+++ client/v1/request.js
@@ -296,7 +296,7 @@ Request.prototype._mergeOptions = function(options) {
 
 
 Request.prototype.parseMiddleware = function (response) {
-    if(response.statusCode===201){
+    if(response.req._headers.host==='upload.instagram.com' && response.statusCode===201){
         var loaded = /(\d+)-(\d+)\/(\d+)/.exec(response.body);
         response.body = {status:"ok",start:loaded[1],end:loaded[2],total:loaded[3]};
         return response;
