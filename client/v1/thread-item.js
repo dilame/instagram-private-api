47ef085dfeff4daca06bac73e94f77a083a928fb
Pablo Matías Gomez <pablomatiasgomez@gmail.com>
2018-01-12 17:11:21 -0300

    added support for raven media

--- client/v1/thread-item.js
+++ client/v1/thread-item.js
@@ -36,10 +36,19 @@ ThreadItem.prototype.parseParams = function (json) {
         hash.text = json.text;
     }
     if (hash.type === "media") {
-        var media = {};
-        media.images = json.media.image_versions2.candidates;
-        media.mediaType = json.media.media_type;
-        hash.media = media;
+        var directMedia = {};
+        directMedia.mediaType = json.media.media_type;
+        directMedia.images = json.media.image_versions2 ? json.media.image_versions2.candidates : null;
+        directMedia.videos = json.media.video_versions;
+        hash.directMedia = directMedia;
+    }
+    if (hash.type === "raven_media") {
+        hash.type = 'ravenMedia';
+        var directStory = {};
+        directStory.mediaType = json.raven_media.media_type;
+        directStory.images = json.raven_media.image_versions2 ? json.raven_media.image_versions2.candidates : null;
+        directStory.videos = json.raven_media.video_versions;
+        hash.directStory = directStory;
     }
     if (hash.type === "media_share") {
         hash.type = 'mediaShare';
