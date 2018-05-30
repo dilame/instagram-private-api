ca487d2d25d0e4df8606961c71af6fee4e33e52f
Samarth <samarthdave@users.noreply.github.com>
2018-05-30 14:25:54 -0500

    Updated support for getAllCookies

--- client/v1/cookie-file-storage.js
+++ client/v1/cookie-file-storage.js
@@ -5,17 +5,25 @@ var fs = require('fs');
 var _ = require('lodash');
 var Helpers = require('../../helpers');
 var CookieStorage = require('./cookie-storage');
-
+var CONSTANTS = require("./constants");
 
 function CookieFileStorage(cookiePath) {
     cookiePath = path.resolve(cookiePath);
     Helpers.ensureExistenceOfJSONFilePath(cookiePath);
-    CookieStorage.call(this, new FileCookieStore(cookiePath))
+    var store = new FileCookieStore(cookiePath);
+    store.__proto__.getAllCookies = function (cb) {
+      store.findCookies(CONSTANTS.HOSTNAME, '/', function (err, cookies) {
+        cookies.sort(function (a, b) {
+          return (a.creationIndex || 0) - (b.creationIndex || 0);
+        });
+        cb(null, cookies);
+      });
+    }
+    CookieStorage.call(this, store);
 }
 
 util.inherits(CookieFileStorage, CookieStorage);
 module.exports = CookieFileStorage;
-
 
 CookieFileStorage.prototype.destroy = function(){
     fs.unlinkSync(this.storage.filePath);
