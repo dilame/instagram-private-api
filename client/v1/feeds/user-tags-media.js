5e02f9d654a5feff4f1c41e47a3dc84853d4ca81
Karl Horky <karl.horky@gmail.com>
2018-07-08 22:58:47 +0200

    Fix next_max_id

--- client/v1/feeds/user-tags-media.js
+++ client/v1/feeds/user-tags-media.js
@@ -26,10 +26,9 @@ UserTagsMediaFeed.prototype.get = function () {
                 })
                 .send()
                 .then(function(data) {
-                    that.moreAvailable = data.more_available;
-                    var lastOne = _.last(data.items);
-                    if (that.moreAvailable && lastOne) {
-                        that.setCursor(lastOne.id);
+                    that.moreAvailable = !!data.next_max_id;
+                    if (that.moreAvailable) {
+                        that.setCursor(data.next_max_id);
                     }
                     return _.map(data.items, function (medium) {
                         return new Media(that.session, medium);
