e10ad7c67aaaff55c8d49dce918925b581900e70
bowzee <dilame.bowzee@gmail.com>
2019-02-22 01:52:37 +0300

    Update user model

--- src/models/abstract.model.ts
+++ src/models/abstract.model.ts
@@ -2,6 +2,7 @@ import * as camelKeys from 'camelcase-keys';
 import { classToPlain } from 'class-transformer';
 
 export abstract class AbstractModel {
+  [x: string]: any;
   get params() {
     return camelKeys(classToPlain(this), { deep: true });
   }
