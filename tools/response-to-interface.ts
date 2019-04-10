e249f79ab6cf4cee329c03fe3927c1071885cb5a
bowzee <dilame.nickolson@gmail.com>
2019-04-10 23:23:09 +0300

    Create auto response-generation tool

---
+++ tools/response-to-interface.ts
@@ -0,0 +1,29 @@
+import 'dotenv/config';
+import { IgApiClient } from '../src';
+import * as Bluebird from 'bluebird';
+import { writeFile } from 'fs';
+import { json2ts } from 'json-ts/dist';
+import { camelCase } from 'lodash';
+
+// Modify this function as you wish to get needed json
+async function getResponse() {
+  const ig = new IgApiClient();
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  ig.state.proxyUrl = process.env.IG_PROXY;
+  const auth = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  return await ig.feed.user(auth.pk).request();
+}
+
+(async function createInterface(outputName: string) {
+  const json = await getResponse();
+  const camelCasedOutputName = camelCase(outputName);
+  let interfaces = json2ts(JSON.stringify(json), {
+    prefix: camelCasedOutputName.charAt(0).toUpperCase() + camelCasedOutputName.slice(1) + 'Response',
+  });
+  interfaces = interfaces.replace(/interface/g, 'export interface');
+  await Bluebird.fromCallback(cb => writeFile(`./src/responses/${outputName}.response.ts`, interfaces, cb));
+  console.log('Success');
+})(
+  // And pass output name
+  'user.feed',
+);
