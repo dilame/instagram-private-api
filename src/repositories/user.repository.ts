f3c91fe4fb9e81ec652da745f5fc74bbb075ec21
bowzee <dilame.nickolson@gmail.com>
2019-04-14 23:45:17 +0300

    (User) fix user-info endpoint

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -9,7 +9,7 @@ import {
 export class UserRepository extends InstagramRepository {
   async info(id: string | number): Promise<UserRepositoryInfoResponseUser> {
     const { body } = await this.client.request.send<UserRepositoryInfoResponseRootObject>({
-      url: `/api/v1/users/${id}/info`,
+      url: `/api/v1/users/${id}/info/`,
     });
     return body.user;
   }
