7ff7ee903fb9b0c5021d5ad31fe6128b2ec913c7
bowzee <dilame.nickolson@gmail.com>
2019-04-14 20:19:19 +0300

    Change IgResponseError message format

--- src/errors/ig-response.error.ts
+++ src/errors/ig-response.error.ts
@@ -4,9 +4,9 @@ import { Response } from 'request';
 export class IgResponseError<TBody extends { [x: string]: any } = any> extends IgClientError {
   constructor(public response: Pick<Response, Exclude<keyof Response, 'body'>> & { body: TBody }) {
     super(
-      `${response.body.message || ''}; ${response.request.method} ${response.request.uri.path} - ${
-        response.statusCode
-      } ${response.statusMessage}`,
+      `${response.request.method} ${response.request.uri.path} - ${response.statusCode} ${
+        response.statusMessage
+      }; ${response.body.message || ''}`,
     );
   }
 }
