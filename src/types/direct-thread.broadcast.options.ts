3ddce7f52b135f2bda2df43679e7a07bf0ec031a
bowzee <dilame.nickolson@gmail.com>
2019-05-02 15:11:40 +0300

    (DirectThreadEntity) add ability to create thread

--- src/types/direct-thread.broadcast.options.ts
+++ src/types/direct-thread.broadcast.options.ts
@@ -1,6 +1,16 @@
-export interface DirectThreadBroadcastOptions {
-  threadIds?: Array<string | number> | string | number;
-  userIds?: Array<string | number> | string | number;
+import { XOR } from 'ts-xor';
+
+type ExistingThreadOptions = {
+  threadIds: Array<string | number> | string | number;
+};
+type CreateThreadOptions = {
+  userIds: Array<string | number> | string | number;
+};
+
+type DirectTreadBroadcastBaseOptions = {
   item: string;
   form?: { [x: string]: any };
-}
+};
+
+export type DirectThreadBroadcastOptions = DirectTreadBroadcastBaseOptions &
+  XOR<ExistingThreadOptions, CreateThreadOptions>;
