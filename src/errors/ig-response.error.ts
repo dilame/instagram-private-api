821476db614362beacb512f3903c37fede91f914
bowzee <dilame.nickolson@gmail.com>
2019-05-07 02:02:32 +0300

    (IgClientError) prettify error messages By making other properties non-enumerable

--- src/errors/ig-response.error.ts
+++ src/errors/ig-response.error.ts
@@ -2,11 +2,18 @@ import { IgClientError } from './ig-client.error';
 import { IgResponse } from '../types/ig-response';
 
 export class IgResponseError<TBody extends { [x: string]: any } = any> extends IgClientError {
-  constructor(public response: IgResponse<TBody>, public text = response.body.message) {
+  public text: string;
+  constructor(public response: IgResponse<TBody>) {
     super(
       `${response.request.method} ${response.request.uri.path} - ${response.statusCode} ${
         response.statusMessage
-      }; ${text || ''}`,
+      }; ${response.body.message || ''}`,
     );
+    if (response.body.message) {
+      this.text = response.body.message;
+    }
+    Object.defineProperty(this, 'response', {
+      enumerable: false,
+    });
   }
 }
