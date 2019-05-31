1237d629250d0af66ca28af7f876779a890399c3
Matthew Bergwall <mbergwall2222@Matthews-MacBook-Pro.local>
2019-05-31 12:19:48 -0400

    add pending friendships feed

--- src/core/feed.factory.ts
+++ src/core/feed.factory.ts
@@ -2,6 +2,7 @@ import { IgApiClient } from './client';
 import {
   AccountFollowersFeed,
   AccountFollowingFeed,
+  AccountFriendshipsFeed,
   DirectInboxFeed,
   DirectThreadFeed,
   LocationFeed,
@@ -26,6 +27,11 @@ export class FeedFactory {
   }
   public accountFollowing(id?: string | number): AccountFollowingFeed {
     const feed = new AccountFollowingFeed(this.client);
+    feed.id = id || this.client.state.cookieUserId;
+    return feed;
+  }
+  public accountPendingFriendships(id?: string | number): AccountFriendshipsFeed {
+    const feed = new AccountFriendshipsFeed(this.client);
     feed.id = id || this.client.state.cookieUserId;
     return feed;
   }
