32257128a942dc18e3a657af5920bee2788f5b44
realinstadude <50342088+realinstadude@users.noreply.github.com>
2019-06-12 22:32:46 +0300

    (Examples) SOCKS5 example simpler proxy settings

--- examples/socks5-proxy.example.ts
+++ examples/socks5-proxy.example.ts
@@ -7,8 +7,8 @@ const shttps = require('socks5-https-client/lib/Agent'); // you should install S
   ig.state.generateDevice(process.env.IG_USERNAME);
   ig.request.defaults.agentClass = shttps; // apply agent class to request library defaults
   ig.request.defaults.agentOptions = {
-      socksHost: process.env.IG_PROXY.split(':')[0], // proxy hostname
-      socksPort: parseInt(process.env.IG_PROXY.split(':')[1]) // proxy port
+      socksHost: process.env.IG_PROXY_HOSTNAME, // proxy hostname
+      socksPort: process.env.IG_PROXY_POST // proxy port
   };
   // Now we can perform authorization using our SOCKS5 proxy.
   const auth = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
