65a84a4bc9f1ba0248501cef16cd498a6bc2cb84
Nerixyz <nero.9@hotmail.de>
2019-06-23 12:56:57 +0200

    updated examples

--- examples/user-stories-watching.example.ts
+++ examples/user-stories-watching.example.ts
@@ -1,3 +1,4 @@
+/* tslint:disable:no-console */
 import 'dotenv/config';
 import { IgApiClient } from '../src';
 
@@ -6,13 +7,19 @@ import { IgApiClient } from '../src';
   ig.state.generateDevice(process.env.IG_USERNAME);
   ig.state.proxyUrl = process.env.IG_PROXY;
   const auth = await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  console.log(JSON.stringify(auth));
+
   const targetUser = await ig.user.searchExact('username'); // getting exact user by login
   const reelsFeed = ig.feed.reelsMedia({ // working with reels media feed (stories feed)
-    userIds: [targetUser.pk] // you can specify multiple user id's, "pk" param is user id
+    userIds: [targetUser.pk], // you can specify multiple user id's, "pk" param is user id
   });
   const storyItems = await reelsFeed.items(); // getting reels, see "account-followers.feed.example.ts" if you want to know how to work with feeds
-  if (storyItems.length === 0) // we can check items length and find out if user does not have any story to watch
-    return false;
-  const seenResult = await ig.story.seen([storyItems[0]]); // now we can mark story as seen using "story" service, you can specify multiple stories, in this case we watching only first story
-  console.log(seenResult); // seenResult.status should be "ok"
+  if (storyItems.length === 0) {// we can check items length and find out if the user does have any story to watch
+    console.log(`${targetUser.username} doesn't have any story`);
+    return;
+  }
+  const seenResult = await ig.story.seen([storyItems[0]]);
+  // now we can mark story as seen using story-service, you can specify multiple stories, in this case we are only watching the first story
+
+  console.log(seenResult.status); // seenResult.status should be "ok"
 })();
