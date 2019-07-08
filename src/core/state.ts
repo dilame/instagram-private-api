6cf9edbaaf1b4bb73a4ddcd8d94a78975664a864
Nerixyz <nero.9@hotmail.de>
2019-07-08 22:20:59 +0200

    revert capabilities

--- src/core/state.ts
+++ src/core/state.ts
@@ -37,7 +37,7 @@ export class State {
   language: string = 'en_US';
   timezoneOffset: string = String(new Date().getTimezoneOffset() * -60);
   radioType = 'wifi-none';
-  capabilitiesHeader = '3brTvw==';
+  capabilitiesHeader = '3brTPw==';
   connectionTypeHeader = 'WIFI';
   deviceString: string;
   build: string;
