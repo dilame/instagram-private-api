7f4192315751428732c7827ae8e7f89d5394aadf
Mauricio <mauriciojs.junior@gmail.com>
2019-07-10 12:47:30 -0300

    Create pagination and limit inbox example

---
+++ examples/dm-thread-pagination.example.ts
@@ -0,0 +1,16 @@
+import { IgApiClient } from '../src';
+
+(async () => {
+  const ig = new IgApiClient();
+  ig.state.generateDevice(process.env.IG_USERNAME);
+  await ig.account.login(process.env.IG_USERNAME, process.env.IG_PASSWORD);
+  const inboxFeed = ig.feed.directInbox();
+  //set the limit threads per request. default 20
+  inboxFeed.limit = 10;
+  do {
+    let threads = await inboxFeed.items();
+    threads.forEach(element => {
+      console.log(element['users'][0]['username']);
+    });
+  } while (inboxFeed.moreAvailable);
+})();
