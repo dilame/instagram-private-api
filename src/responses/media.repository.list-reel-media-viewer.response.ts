082254444b2e7580a915f2dfa666bd5e5e67ee49
Nerixyz <nero.9@hotmail.de>
2019-09-05 17:24:09 +0200

    fix ListReelMediaViewerResponse

--- src/responses/media.repository.list-reel-media-viewer.response.ts
+++ src/responses/media.repository.list-reel-media-viewer.response.ts
@@ -1,4 +1,31 @@
 export interface MediaRepositoryListReelMediaViewerResponseRootObject {
+    users: MediaRepositoryListReelMediaViewerResponseUsersItem[];
+    next_max_id: null;
+    user_count: number;
+    total_viewer_count: number;
+    updated_media: MediaRepositoryListReelMediaViewerResponseUpdated_media;
+    status: string;
+}
+export interface MediaRepositoryListReelMediaViewerResponseUsersItem {
+    pk: number;
+    username: string;
+    full_name: string;
+    is_private: boolean;
+    profile_pic_url: string;
+    profile_pic_id: string;
+    friendship_status?: MediaRepositoryListReelMediaViewerResponseFriendship_status;
+    is_verified: boolean;
+    latest_reel_media?: number;
+}
+export interface MediaRepositoryListReelMediaViewerResponseFriendship_status {
+    following: boolean;
+    is_private: boolean;
+    incoming_request: boolean;
+    outgoing_request: boolean;
+    is_bestie: boolean;
+    is_restricted: boolean;
+}
+export interface MediaRepositoryListReelMediaViewerResponseUpdated_media {
     taken_at: number;
     pk: string;
     id: string;
@@ -24,9 +51,15 @@ export interface MediaRepositoryListReelMediaViewerResponseRootObject {
     can_reshare: boolean;
     can_reply: boolean;
     is_pride_media: boolean;
+    story_hashtags: MediaRepositoryListReelMediaViewerResponseStoryHashtagsItem[];
+    creative_config: MediaRepositoryListReelMediaViewerResponseCreative_config;
+    reel_mentions: MediaRepositoryListReelMediaViewerResponseReelMentionsItem[];
+    story_locations: MediaRepositoryListReelMediaViewerResponseStoryLocationsItem[];
     story_is_saved_to_archive: boolean;
-    story_quizs: MediaRepositoryListReelMediaViewerResponseStoryQuizsItem[];
-    story_quiz_participant_infos: MediaRepositoryListReelMediaViewerResponseStoryQuizParticipantInfosItem[];
+    story_fundraisers: MediaRepositoryListReelMediaViewerResponseStoryFundraisersItem[];
+    story_fundraiser_donation_infos: MediaRepositoryListReelMediaViewerResponseStoryFundraiserDonationInfosItem[];
+    story_chats: MediaRepositoryListReelMediaViewerResponseStoryChatsItem[];
+    story_chat_request_infos: MediaRepositoryListReelMediaViewerResponseStoryChatRequestInfosItem[];
     viewers: MediaRepositoryListReelMediaViewerResponseViewersItem[];
     viewer_count: number;
     viewer_cursor: null;
@@ -35,19 +68,6 @@ export interface MediaRepositoryListReelMediaViewerResponseRootObject {
     supports_reel_reactions: boolean;
     show_one_tap_fb_share_tooltip: boolean;
     has_shared_to_fb: number;
-    creative_config: MediaRepositoryListReelMediaViewerResponseCreative_config;
-    story_questions: MediaRepositoryListReelMediaViewerResponseStoryQuestionsItem[];
-    story_question_responder_infos: MediaRepositoryListReelMediaViewerResponseStoryQuestionResponderInfosItem[];
-    story_polls: MediaRepositoryListReelMediaViewerResponseStoryPollsItem[];
-    story_poll_voter_infos: MediaRepositoryListReelMediaViewerResponseStoryPollVoterInfosItem[];
-    story_countdowns: MediaRepositoryListReelMediaViewerResponseStoryCountdownsItem[];
-    story_hashtags: MediaRepositoryListReelMediaViewerResponseStoryHashtagsItem[];
-    reel_mentions: MediaRepositoryListReelMediaViewerResponseReelMentionsItem[];
-    story_locations: MediaRepositoryListReelMediaViewerResponseStoryLocationsItem[];
-    story_fundraisers: MediaRepositoryListReelMediaViewerResponseStoryFundraisersItem[];
-    story_fundraiser_donation_infos: MediaRepositoryListReelMediaViewerResponseStoryFundraiserDonationInfosItem[];
-    story_chats: MediaRepositoryListReelMediaViewerResponseStoryChatsItem[];
-    story_chat_request_infos: MediaRepositoryListReelMediaViewerResponseStoryChatRequestInfosItem[];
 }
 export interface MediaRepositoryListReelMediaViewerResponseImage_versions2 {
     candidates: MediaRepositoryListReelMediaViewerResponseCandidatesItem[];
@@ -73,7 +93,6 @@ export interface MediaRepositoryListReelMediaViewerResponseUser {
     reel_auto_archive?: string;
     is_unpublished?: boolean;
     allowed_commenter_type?: string;
-    friendship_status?: MediaRepositoryListReelMediaViewerResponseFriendship_status;
     media_count?: number;
     follower_count?: number;
     following_count?: number;
@@ -111,206 +130,6 @@ export interface MediaRepositoryListReelMediaViewerResponseUser {
 export interface MediaRepositoryListReelMediaViewerResponseFb_user_tags {
     'in': any[];
 }
-export interface MediaRepositoryListReelMediaViewerResponseStoryQuizsItem {
-    x: string;
-    y: number;
-    z: number;
-    width: string;
-    height: number;
-    rotation: number;
-    is_pinned: number;
-    is_hidden: number;
-    is_sticker: number;
-    quiz_sticker: MediaRepositoryListReelMediaViewerResponseQuiz_sticker;
-}
-export interface MediaRepositoryListReelMediaViewerResponseQuiz_sticker {
-    id: string;
-    quiz_id: string;
-    question: string;
-    tallies: MediaRepositoryListReelMediaViewerResponseTalliesItem[];
-    correct_answer: number;
-    viewer_can_answer: boolean;
-    finished: boolean;
-    text_color: string;
-    start_background_color: string;
-    end_background_color: string;
-}
-export interface MediaRepositoryListReelMediaViewerResponseTalliesItem {
-    text: string;
-    count: number;
-    font_size?: number;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryQuizParticipantInfosItem {
-    quiz_id: string;
-    participants: MediaRepositoryListReelMediaViewerResponseParticipantsItem[];
-    max_id: null;
-    more_available: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseParticipantsItem {
-    user: MediaRepositoryListReelMediaViewerResponseUser;
-    answer: number;
-    ts: number;
-}
-export interface MediaRepositoryListReelMediaViewerResponseViewersItem {
-    pk: number;
-    username: string;
-    full_name: string;
-    is_private: boolean;
-    profile_pic_url: string;
-    profile_pic_id: string;
-    is_verified: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseCreative_config {
-    capture_type: string;
-    camera_facing: string;
-    should_render_try_it_on: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryQuestionsItem {
-    x: string;
-    y: number;
-    z: number;
-    width: string;
-    height: number;
-    rotation: number;
-    is_pinned: number;
-    is_hidden: number;
-    is_sticker: number;
-    question_sticker: MediaRepositoryListReelMediaViewerResponseQuestion_sticker;
-}
-export interface MediaRepositoryListReelMediaViewerResponseQuestion_sticker {
-    question_type: string;
-    question_id: string;
-    question: string;
-    media_id: string;
-    text_color: string;
-    background_color: string;
-    viewer_can_interact: boolean;
-    profile_pic_url: string;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryQuestionResponderInfosItem {
-    question_id: string;
-    question: string;
-    question_type: string;
-    background_color: string;
-    text_color: string;
-    responders: MediaRepositoryListReelMediaViewerResponseRespondersItem[];
-    max_id: null;
-    more_available: boolean;
-    question_response_count: number;
-    unanswered_response_count: number;
-    latest_question_response_time: number;
-}
-export interface MediaRepositoryListReelMediaViewerResponseRespondersItem {
-    response: string;
-    has_shared_response: boolean;
-    id: string;
-    user: MediaRepositoryListReelMediaViewerResponseUser;
-    ts: number;
-    music_response: MediaRepositoryListReelMediaViewerResponseMusic_response;
-}
-export interface MediaRepositoryListReelMediaViewerResponseMusic_response {
-    music_asset_info: MediaRepositoryListReelMediaViewerResponseMusic_asset_info;
-    music_consumption_info: MediaRepositoryListReelMediaViewerResponseMusic_consumption_info;
-}
-export interface MediaRepositoryListReelMediaViewerResponseMusic_asset_info {
-    id: string;
-    title: string;
-    subtitle: string;
-    display_artist: string;
-    cover_artwork_uri: string;
-    cover_artwork_thumbnail_uri: string;
-    progressive_download_url: string;
-    highlight_start_times_in_ms: number[];
-    is_explicit: boolean;
-    dash_manifest: string;
-    has_lyrics: boolean;
-    audio_asset_id: string;
-}
-export interface MediaRepositoryListReelMediaViewerResponseMusic_consumption_info {
-    ig_artist: MediaRepositoryListReelMediaViewerResponseIg_artist;
-    placeholder_profile_pic_url: string;
-    should_mute_audio: boolean;
-    should_mute_audio_reason: string;
-    overlap_duration_in_ms: null;
-    audio_asset_start_time_in_ms: null;
-}
-export interface MediaRepositoryListReelMediaViewerResponseIg_artist {
-    pk: number;
-    username: string;
-    full_name: string;
-    is_private: boolean;
-    profile_pic_url: string;
-    profile_pic_id: string;
-    is_verified: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryPollsItem {
-    x: string;
-    y: number;
-    z: number;
-    width: string;
-    height: number;
-    rotation: number;
-    is_pinned: number;
-    is_hidden: number;
-    is_sticker: number;
-    poll_sticker: MediaRepositoryListReelMediaViewerResponsePoll_sticker;
-}
-export interface MediaRepositoryListReelMediaViewerResponsePoll_sticker {
-    id: string;
-    poll_id: string;
-    question: string;
-    tallies: MediaRepositoryListReelMediaViewerResponseTalliesItem[];
-    promotion_tallies: null;
-    viewer_can_vote: boolean;
-    is_shared_result: boolean;
-    finished: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryPollVoterInfosItem {
-    poll_id: string;
-    voters: MediaRepositoryListReelMediaViewerResponseVotersItem[];
-    max_id: null;
-    more_available: boolean;
-    latest_poll_vote_time: number;
-}
-export interface MediaRepositoryListReelMediaViewerResponseVotersItem {
-    user: MediaRepositoryListReelMediaViewerResponseUser;
-    vote: number;
-    ts: number;
-}
-export interface MediaRepositoryListReelMediaViewerResponseFriendship_status {
-    following: boolean;
-    is_private: boolean;
-    incoming_request: boolean;
-    outgoing_request: boolean;
-    is_bestie: boolean;
-    is_restricted: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseStoryCountdownsItem {
-    x: string;
-    y: number;
-    z: number;
-    width: string;
-    height: number;
-    rotation: number;
-    is_pinned: number;
-    is_hidden: number;
-    is_sticker: number;
-    countdown_sticker: MediaRepositoryListReelMediaViewerResponseCountdown_sticker;
-}
-export interface MediaRepositoryListReelMediaViewerResponseCountdown_sticker {
-    countdown_id: string;
-    end_ts: number;
-    text: string;
-    text_color: string;
-    start_background_color: string;
-    end_background_color: string;
-    digit_color: string;
-    digit_card_color: string;
-    following_enabled: boolean;
-    is_owner: boolean;
-    attribution: null;
-    viewer_is_following: boolean;
-}
 export interface MediaRepositoryListReelMediaViewerResponseStoryHashtagsItem {
     x: number;
     y: number;
@@ -326,6 +145,11 @@ export interface MediaRepositoryListReelMediaViewerResponseStoryHashtagsItem {
 export interface MediaRepositoryListReelMediaViewerResponseHashtag {
     name: string;
     id: string;
+}
+export interface MediaRepositoryListReelMediaViewerResponseCreative_config {
+    capture_type: string;
+    camera_facing: string;
+    should_render_try_it_on: boolean;
 }
 export interface MediaRepositoryListReelMediaViewerResponseReelMentionsItem {
     x: number;
@@ -457,7 +281,9 @@ export interface MediaRepositoryListReelMediaViewerResponseStoryChatRequestInfos
     total_thread_participants: number;
     total_participant_requests: number;
 }
-export interface MediaRepositoryListReelMediaViewerResponseUsersItem {
+export interface MediaRepositoryListReelMediaViewerResponseRequester_usernames {
+}
+export interface MediaRepositoryListReelMediaViewerResponseViewersItem {
     pk: number;
     username: string;
     full_name: string;
@@ -465,6 +291,4 @@ export interface MediaRepositoryListReelMediaViewerResponseUsersItem {
     profile_pic_url: string;
     profile_pic_id: string;
     is_verified: boolean;
-}
-export interface MediaRepositoryListReelMediaViewerResponseRequester_usernames {
 }
