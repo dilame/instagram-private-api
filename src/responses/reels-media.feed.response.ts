21ded0f3c6db7b9de4039ab6dbf3e4863335b800
envy124 <pkgsail.dev@gmail.com>
2019-12-12 15:45:52 +0300

    Add ReelsMediaFeedResponsePollSticker viewer_vote

--- src/responses/reels-media.feed.response.ts
+++ src/responses/reels-media.feed.response.ts
@@ -97,6 +97,7 @@ export interface ReelsMediaFeedResponsePollSticker {
   tallies: ReelsMediaFeedResponseTalliesItem[];
   promotion_tallies: null;
   viewer_can_vote: boolean;
+  viewer_vote: number;
   is_shared_result: boolean;
   finished: boolean;
 }
