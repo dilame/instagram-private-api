3ba4624bccc07d4838154cf244bb1e3aeb99f5d8
Nerixyz <nero.9@hotmail.de>
2019-12-22 18:38:43 +0100

    added DirectThreadEntity.broadcastVoice()

--- src/responses/direct-thread.repository.broadcast.response.ts
+++ src/responses/direct-thread.repository.broadcast.response.ts
@@ -2,7 +2,15 @@ export interface DirectThreadRepositoryBroadcastResponseRootObject {
   action: string;
   status_code: string;
   payload: DirectThreadRepositoryBroadcastResponsePayload;
+  message_metadata: DirectThreadRepositoryBroadcastResponseMessage_metadata[];
   status: string;
+}
+export interface DirectThreadRepositoryBroadcastResponseMessage_metadata {
+  client_context: string;
+  item_id: string;
+  timestamp: string;
+  thread_id: string;
+  participant_ids: string[];
 }
 export interface DirectThreadRepositoryBroadcastResponsePayload {
   client_context: string;
