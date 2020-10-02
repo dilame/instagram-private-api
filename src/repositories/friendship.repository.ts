d867d8d6481ba282b7a94c005d47dc05b35b431c
Nerixyz <nero.9@hotmail.de>
2020-10-02 19:43:53 +0200

    feat: add unmutePostsOrStoryFromFollow()

--- src/repositories/friendship.repository.ts
+++ src/repositories/friendship.repository.ts
@@ -91,21 +91,37 @@ export class FriendshipRepository extends Repository {
     return body.friendship_statuses;
   }
 
-  async mutePostsOrStoryFromFollow(options: {
+  mutePostsOrStoryFromFollow(options: {
     mediaId?: string;
     targetReelAuthorId?: string;
     targetPostsAuthorId?: string;
   }): Promise<FriendshipRepositoryChangeResponseRootObject> {
+    return this.changeMuteFromFollow('mute', {
+      media_id: options.mediaId,
+      target_reel_author_id: options.targetReelAuthorId,
+      target_posts_author_id: options.targetPostsAuthorId,
+    });
+  }
+
+  unmutePostsOrStoryFromFollow(options: {
+    targetReelAuthorId?: string;
+    targetPostsAuthorId?: string;
+  }): Promise<FriendshipRepositoryChangeResponseRootObject> {
+    return this.changeMuteFromFollow('unmute', {
+      target_reel_author_id: options.targetReelAuthorId,
+      target_posts_author_id: options.targetPostsAuthorId,
+    });
+  }
+
+  private async changeMuteFromFollow(mode: 'mute' | 'unmute', options: Record<string, any>) {
     const { body } = await this.client.request.send({
-      url: `/api/v1/friendships/mute_posts_or_story_from_follow/`,
+      url: `/api/v1/friendships/${mode}_posts_or_story_from_follow/`,
       method: 'POST',
       form: {
         _csrftoken: this.client.state.cookieCsrfToken,
         _uid: this.client.state.cookieUserId,
         _uuid: this.client.state.uuid,
-        media_id: options.mediaId,
-        target_reel_author_id: options.targetReelAuthorId,
-        target_posts_author_id: options.targetPostsAuthorId,
+        ...options,
       },
     });
     return body;
