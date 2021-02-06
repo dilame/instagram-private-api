1fb06af318aecd8a487685f538d88495e3427c6e
Olivier Mouren <mouren.olivier@gmail.com>
2021-02-06 01:24:39 +0100

    Force user-agent for archive live endpoint

--- src/repositories/archive.repository.ts
+++ src/repositories/archive.repository.ts
@@ -6,6 +6,9 @@ export class ArchiveRepository extends Repository {
     const { body } = await this.client.request.send<ArchiveRepositoryLivesResponseRootObject>({
       url: '/api/v1/archive/live/lives_archived/',
       method: 'GET',
+      headers: {
+        'User-Agent': `Instagram 172.0.0.21.123 Android (${this.client.state.deviceString}; ${this.client.state.language}; 269790810)`
+      }
     });
 
     return body;
