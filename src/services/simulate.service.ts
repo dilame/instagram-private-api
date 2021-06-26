0c7e8cd31a378211a7d9b5412987103b26296da0
Alecsander Hoppe <alecsanderhoppe@gmail.com>
2021-06-26 20:19:32 -0300

    Remove logAttribution function until request is fixed

--- src/services/simulate.service.ts
+++ src/services/simulate.service.ts
@@ -11,7 +11,6 @@ export class SimulateService extends Repository {
       () => this.client.account.contactPointPrefill('prefill').catch(() => undefined),
       () => this.client.launcher.preLoginSync(),
       () => this.client.qe.syncLoginExperiments(),
-      () => this.client.attribution.logAttribution(),
       () => this.client.account.getPrefillCandidates().catch(() => undefined),
     ];
   }
@@ -21,7 +20,6 @@ export class SimulateService extends Repository {
       () => this.client.zr.tokenResult(),
       () => this.client.launcher.postLoginSync(),
       () => this.client.qe.syncExperiments(),
-      () => this.client.attribution.logAttribution(),
       () => this.client.attribution.logResurrectAttribution(),
       () => this.client.loom.fetchConfig(),
       () => this.client.linkedAccount.getLinkageStatus(),
