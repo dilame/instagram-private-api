fc789643d6e773aa4314f650ba451f58779cd698
s.reza <rezasefidgar78@gmail.com>
2021-11-03 10:25:29 +0330

    Update packages, Update version to 136

--- src/core/state.ts
+++ src/core/state.ts
@@ -226,7 +226,7 @@ export class State {
     const obj = typeof state === 'string' ? JSON.parse(state) : state;
     if (typeof obj !== 'object') {
       State.stateDebug(`State deserialization failed, obj is of type ${typeof obj} (object expected)`);
-      throw new TypeError('State isn\'t an object or serialized JSON');
+      throw new TypeError("State isn't an object or serialized JSON");
     }
     State.stateDebug(`Deserializing ${Object.keys(obj).join(', ')}`);
     if (obj.constants) {
