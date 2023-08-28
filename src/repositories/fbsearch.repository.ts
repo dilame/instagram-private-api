808e0f066a4814858e900e702204f3c9ae32f8e6
Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>
2023-08-28 22:00:46 +0330

    fix: fix error after login (IgNotFoundError: GET /api/v1/fbsearch/suggested_searches/?type=blended - 404 Not Found)
    - that make application to crash
    
    Signed-off-by: Abolfazl Nasiri Almas <almas.abolfazl@gmail.com>

--- src/repositories/fbsearch.repository.ts
+++ src/repositories/fbsearch.repository.ts
@@ -6,42 +6,58 @@ import {
 
 export class FbsearchRepository extends Repository {
   async suggestedSearches(type: 'blended' | 'users' | 'hashtags' | 'places') {
-    const { body } = await this.client.request.send({
-      url: '/api/v1/fbsearch/suggested_searches/',
-      qs: {
-        type,
-      },
-    });
-    return body;
+    try {
+      const { body } = await this.client.request.send({
+        url: '/api/v1/fbsearch/suggested_searches/',
+        qs: {
+          type,
+        },
+      });
+      return body;
+    } catch (error) {
+      return null;
+    }
   }
   async recentSearches() {
-    const { body } = await this.client.request.send({
-      url: '/api/v1/fbsearch/recent_searches/',
-    });
-    return body;
+    try {
+      const { body } = await this.client.request.send({
+        url: '/api/v1/fbsearch/recent_searches/',
+      });
+      return body;
+    } catch (error) {
+      return null;
+    }
   }
 
   async topsearchFlat(query: string): Promise<FbsearchRepositoryTopsearchFlatResponseRootObject> {
-    const { body } = await this.client.request.send<FbsearchRepositoryTopsearchFlatResponseRootObject>({
-      url: '/api/v1/fbsearch/topsearch_flat/',
-      qs: {
-        timezone_offset: this.client.state.timezoneOffset,
-        count: 30,
-        query,
-        context: 'blended',
-      },
-    });
-    return body;
+    try {
+      const { body } = await this.client.request.send<FbsearchRepositoryTopsearchFlatResponseRootObject>({
+        url: '/api/v1/fbsearch/topsearch_flat/',
+        qs: {
+          timezone_offset: this.client.state.timezoneOffset,
+          count: 30,
+          query,
+          context: 'blended',
+        },
+      });
+      return body;
+    } catch (error) {
+      return null;
+    }
   }
   async places(query: string) {
-    const { body } = await this.client.request.send<FbsearchRepositoryPlacesResponseRootObject>({
-      url: '/api/v1/fbsearch/places/',
-      qs: {
-        timezone_offset: this.client.state.timezoneOffset,
-        count: 30,
-        query,
-      },
-    });
-    return body;
+    try {
+      const { body } = await this.client.request.send<FbsearchRepositoryPlacesResponseRootObject>({
+        url: '/api/v1/fbsearch/places/',
+        qs: {
+          timezone_offset: this.client.state.timezoneOffset,
+          count: 30,
+          query,
+        },
+      });
+      return body;
+    } catch (error) {
+      return null;
+    }
   }
 }
